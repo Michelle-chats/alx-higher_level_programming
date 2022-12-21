b487bb0deb73f62252805d599f1764cd24f4e19b
Michelle-chats <mtchatikobo@gmail.com>
2022-12-21 06:17:41 -0800

    submitting

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-// prints three lines using an array of strings and a loop
-const stringArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let i = 0; i < stringArray.length; i++) {
-  console.log(stringArray[i]);
+const s = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < 3; i++) {
+  console.log(s[i]);
 }
