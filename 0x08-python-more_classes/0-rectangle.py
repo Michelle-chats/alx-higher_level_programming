d6fdc633d7cb3f11e4d6b852c5616e104b6b6926
Michelle-chats <you@example.com>
2023-01-22 01:59:03 +0200

    submitting

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Module 0-rectangle
-Defines an empty Rectangle class
+"""
+New class Rectangle
 """
 
 
 class Rectangle:
-    """an empty Rectangle class"""
+    """ Rectangle class empty """
     pass
