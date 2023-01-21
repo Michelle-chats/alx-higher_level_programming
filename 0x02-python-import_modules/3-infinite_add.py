d98559faf996084e312cd803fb2ac7898f6065bd
Michelle-chats <you@example.com>
2023-01-22 00:51:30 +0200

    submitting

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    result = 0
-    for arg in sys.argv:
-        if arg != sys.argv[0]:
-            result += int(arg)
-    print(result)
+    ans = 0
+    num = (len(sys.argv) - 1)
+    for index in range(1, num + 1):
+        ans += int(sys.argv[index])
+    print("{:d}".format(ans))
