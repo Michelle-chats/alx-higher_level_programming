d69415bee35e06e2c1adbe68f477d33cf91b8ae1
Michelle-chats <you@example.com>
2023-05-08 16:17:24 +0200

    submitting

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import hidden_4
-    for name in dir(hidden_4):
-        if name[0] != '_' and name[1] != '_':
-            print(name)
+import hidden_4
+
+if __name__ == '__main__':
+    def_names = dir(hidden_4)
+
+    for i in range(len(def_names)):
+        if def_names[i][:2] != '__':
+            print(def_names[i])
